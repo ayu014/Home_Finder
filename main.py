840454587265f0dd6ccb466b567c1225f0df0ce7
Ayush Ranjan <ayush0187cse@gmail.com>
2024-12-03 00:52:58 +0530

    Fetch info

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 Zillow = "https://appbrewery.github.io/Zillow-Clone/"
 
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument("--disable-blink-features = AutomationControlled")
@@ -20,9 +21,32 @@ header = {
 class Property:
     def __init__(self):
         self.driver = webdriver.Chrome(options=chrome_options)
+        self.Max_Amount = 3000
+        
     
     def find_home(self):
-        pass
+        with requests.get(url=Zillow,headers=header) as response:
+            # print(response.status_code)
+            content = response.text
+            soup = BeautifulSoup(content,"html.parser")
+            # print(content)
+            all_address_elements = soup.select(".StyledPropertyCardDataWrapper a")
+            all_address = [address.text.replace("|"," ").strip() for address in all_address_elements]
+            # print(all_address)
+
+            all_price_elements = soup.select(".PropertyCardWrapper span")
+            all_price = [price.text.replace("+/mo","").strip() for price in all_price_elements]
+            print(all_price[0])
+
+            
+
+    
+
+search_property = Property()
+search_property.find_home()
+search_property.driver.quit()
+
+
 
 
 
